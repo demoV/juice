1c224c1264705d6e0c0eb9bb54a02eb45d3ca111
Lalit Pandey <lalitpan@thoughtworks.com>
2016-02-13 14:17:59 +0530

    Simple graph completed.
    What changes have you made?
    
    *
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- public/javascript/juiceChart.js
+++ public/javascript/juiceChart.js
@@ -1,13 +1,21 @@
-// var onload = function() {
-// 	setClickEvent();
-// };
+var onload = function() {
+	getAllData();
+};
 
+var drinkData;
 
-var renderGraph = function(dataToRender){
-	d3.select('.graph').remove();
+var getAllData = function(){
+	d3.json("../juice_orders", function(error, json) {
+	  if (error) throw error;
+	  drinkData = json;
+	});
+}
+var renderGraph = function(drinks){
+	console.log(drinks.length)
+	 d3.select('.graph').remove();
 
 	var yScale = d3.scale.linear()
-				.domain([0,300])
+				.domain([0,500])
 				.range([500,100]);
 
 	var xAxis = d3.svg.axis()
@@ -16,31 +24,90 @@ var renderGraph = function(dataToRender){
 
 	var svg = d3.select('body')
 		.append('svg')
-		.attr("class", "axis")
-    	.attr("transform", "translate(0,500)")
-		.call(xAxis)
 		.attr('class','graph')
-		.attr('width', 1000)
-		.attr('height', 600);
+		.attr('width', 1200)
+		.attr('height', 700);
 
 	svg.selectAll('g')
-		.data(dataToRender)	
+		.data(drinks)	
 		.enter()
 		.append('line')
-		.attr('x1',function(d,i){return (i+1)*50})
-		.attr('x2',function(d,i){return (i+1)*50})
+		.attr('x1',function(d,i){return (i+1)*30})
+		.attr('x2',function(d,i){return (i+1)*30})
 		.attr('y1',500)
-		.attr('y2',function(d){return yScale(d.count)})
-		.attr('stroke','black')
+		.attr('y2',function(d){return yScale(d.value)})
+		.attr('stroke','black');
+		
+		d3.selectAll('svg').selectAll('text')
+					.data(drinks)
+					.enter()
+					.append('text')
+					.text(function(d,i){
+						return d.key
+					})
+					.attr('x',function(d,i){return (i+1)*30})
+					.attr('y',510)
+					.attr('stroke','black')
+					.attr('transform', function(d,i){ return 'rotate(90,'+ (i+1)*30 +',510)';});
+		d3.selectAll('svg').selectAll('t')
+					.data(drinks)
+					.enter()
+					.append('text')
+					.text(function(d,i){
+						return d.value
+					})
+					.attr('x',function(d,i){return  (i+1)*30})
+					.attr('y',function(d,i){return yScale(d.value)})
+					.attr('stroke','green')
+					// .attr('transform', function(d,i){ return 'rotate(90,'+ (i+1)*30 +',510)';});
 
 }
 
-
-var getJuiceDataWithoutSugar = function(){
-	$.get('/withoutSugar', function(withoutSugarData){
-		renderGraph(withoutSugarData);
-	});
+var countDrinks = function(drinks){
+	return drinks.reduce(function(initial, perOrder){
+		if(!initial[perOrder.drinkName])
+			initial[perOrder.drinkName] = 0;
+		initial[perOrder.drinkName] += 1;
+		return initial;
+	},{});
+}
+var getWithoutSugarDrinks = function(data){
+	var drinks = withoutSugarDrinks(data);
+	var drinksCount = countDrinks(drinks);
+	var keys = Object.keys(drinksCount);
+	return keys.reduce(function(initial, key){
+		var drink = {key: key, value: drinksCount[key]};
+		initial.push(drink);
+		return initial;
+	},[]);
 };
 
+var getSugarDrinks = function(data){
+	var drinks = DrinksWithSugar(data);
+	var drinksCount = countDrinks(drinks);
+	var keys = Object.keys(drinksCount);
+	return keys.reduce(function(initial, key){
+		var drink = {key: key, value: drinksCount[key]};
+		initial.push(drink);
+		return initial;
+	},[]);
+};
 
-// $(document).ready(onload);
+var DrinksWithSugar = function(){
+	return drinkData.filter(function(perOrder){
+		return !perOrder.isSugarless;
+	});
+};
+var withoutSugarDrinks = function(juiceData){
+	return juiceData.filter(function(perOrder){
+		return perOrder.isSugarless;
+	});
+};
+var showGraphWithoutSugar = function(){
+		renderGraph(getWithoutSugarDrinks(drinkData));
+};
+var showGraphWithSugar = function(){
+	renderGraph(getSugarDrinks(drinkData));	
+}
+
+$(document).ready(onload);
